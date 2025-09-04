51059149f377b9ad84e8a2255ac9bb017ff1a85b
Pravinkumar2604 <pravinkumar@datatechgenius.com>
2025-09-04 19:24:58 +0530

    repository changes in Asset module

---
+++ HRMS.Infrastructure/Repositories/Asset/AssetRepository.cs
@@ -0,0 +1,13 @@
+﻿using HRMS.Infrastructure.Interfaces.Asset;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.Infrastructure.Repositories.Asset
+{
+    public class AssetRepository : IAssetRepository
+    {
+    }
+}
